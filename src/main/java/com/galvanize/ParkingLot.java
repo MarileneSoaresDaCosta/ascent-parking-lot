2d79bdf7c7148688fa183caad3ec4038258cc116
Ginwoo Pak <ginwoopak@gmail.com>
2022-07-22 14:54:13 -0700

    completed up until get methods for ticket system + 6 bullet points done

--- src/main/java/com/galvanize/ParkingLot.java
+++ src/main/java/com/galvanize/ParkingLot.java
@@ -1,15 +1,20 @@
 package com.galvanize;
 
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
+import static java.util.UUID.randomUUID;
+
 public class ParkingLot {
-    // if num of hours > 12, dailyfee
+
     public int dailyFee = 3;
     private int maxSpaces;
     private int hourFee = 5;
-    private HashMap<Integer, ArrayList<String>> tickets = new HashMap<>();
+    private HashMap<String, ArrayList<String>> tickets = new HashMap<>();
     private int[] spaces;
     // array of spaces - 0: empty , 1:busy
     // hashmap
@@ -34,17 +39,67 @@ public class ParkingLot {
         return this.hourFee * hours;
     }
 
-    // issueTicket(ticketNum, allocation, time_entered, license, color){
-    // check if space exists
-    // if so, changes avail
-    // store ticket info -
+    public int checkSpace(){
+        for (int i = 0; i < spaces.length; i++) {
+            if (spaces[i] == 0){
+                spaces[i] = 1;
+                return i + 1;
+            }
+        }
+        return 0;
+    }
 
-    // if no space, return 'full'
+    public void issueTicket( String license ,String color){
+        ArrayList<String> ticket = new ArrayList<>();
+        int space = checkSpace();
+        if(space == 0){
+            System.out.println("Parking full");
+        }
+        String ticketNum = String.valueOf(randomUUID());
+        // check if ticketNum already exists
+        ticket.add(String.valueOf(space));
+        ticket.add(String.valueOf(LocalDateTime.now()));
+        ticket.add(license);
+        ticket.add(color);
+        this.tickets.put(ticketNum, ticket);
 
+    }
 
-    // carExits() {
-//    lookup hash - retrieve info, mark space as empty
-//}
+    public HashMap<String, ArrayList<String>> getTickets(){
+        System.out.println("tickets: " + tickets);
+        System.out.println("spaces: " + Arrays.toString(spaces));
+        return tickets;
+    }
+
+     public void carExits(String ticketNumber, LocalDateTime exitTime) {
+        //    lookup hash - retrieve info, and save to temp var, delete in hash
+         // car info
+         ArrayList<String> carInfo = tickets.get(ticketNumber);
+         System.out.println(carInfo);
+         // delete pair from HashMap
+         tickets.remove(ticketNumber);
+         // mark space as empty
+         // space to empty:
+         String spaceToEmpty= carInfo.get(0);
+         this.spaces[Integer.valueOf(spaceToEmpty)] = 0;
+
+         // calculate hours
+         String timeOfEntryString = carInfo.get(1);
+         LocalDateTime timeOfEntry = LocalDateTime.parse(timeOfEntryString);
+         Duration duration = Duration.between(timeOfEntry, exitTime);
+         System.out.println("duration: " + duration.toMinutes());
+         long minutes = duration.toMinutes(); // 121
+         int hours;
+//         double hours = (Math.toIntExact(minutes));
+        if(minutes % 60 == 0 ) {
+            hours = (int) (minutes / 60);
+        } else {
+            hours = (int) (minutes / 60) + 1;
+        }
+
+         System.out.println("whole hours: " + hours);
+         System.out.println("charge total: " + getCharge(hours));
+}
 
 
 
--- src/test/java/com/galvanize/ParkingLotTest.java
+++ src/test/java/com/galvanize/ParkingLotTest.java
@@ -3,7 +3,11 @@ package com.galvanize;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class ParkingLotTest {
     private ParkingLot parkingLot;
@@ -27,8 +31,42 @@ public class ParkingLotTest {
         assertEquals(expect, actual);
     }
 
+    @Test
+    public void shouldIssueATicketAndReturnTickets() {
+        // issue ticket ("BA235X", "red")
+        parkingLot.issueTicket("BA235X", "red");
+        parkingLot.getTickets();
+    }
 
-//}    @Test
+    @Test
+    public void shouldReturnMessageFullAfter9thCarEnters() {
+        // issue ticket ("BA235X", "red")
+        for (int i = 0; i < 9; i++) {
+            parkingLot.issueTicket("BA235X", "red");
+        }
+        parkingLot.getTickets();  // shows full lot
+    }
+
+    @Test
+    public void shouldUpdateHistoryAndPlacesWhenCarExits() {
+        // cars enter
+        for (int i = 0; i < 8; i++) {
+            parkingLot.issueTicket("BA235X", "red");
+        }
+        // pick first ticket number
+        Set<String> currentTicketsSet = parkingLot.getTickets().keySet();
+        String[] currentTicketsArray = currentTicketsSet.toArray(new String[currentTicketsSet.size()]);
+        System.out.println("first ticket car leaving " + currentTicketsArray[0]);
+        // exitTime
+        LocalDateTime exitTime = LocalDateTime.now().plusHours(2).plusMinutes(1);
+        String carExitTicket = currentTicketsArray[0];
+        parkingLot.carExits(carExitTicket, exitTime);
+        boolean expect = false;
+        boolean actual = parkingLot.getTickets().containsKey(carExitTicket);
+        assertEquals(expect, actual);
+    }
+
+//     @Test
 //     public void shouldReturnDailyFeeBasedOnHoursPassed() {
 //         int expect = 72;
 //         int actual = parkingLot.getCharge(24);
